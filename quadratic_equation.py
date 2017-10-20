e9655f465e47d63fd6bfc746f1f5d4150c124c73
Леонид Гилькович <4aisl.leo@gmail.com>
2017-10-20 13:54:26 +0300

    добавлен gitignore readme

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,10 +4,10 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
-    	return None, None  
-    ferst_root = (-b - sqrt(discriminant)) / (2 * a)
+        return None, None
+    first_root = (-b - sqrt(discriminant)) / (2 * a)
     second_root = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
-        return ferst_root, None  
+        return first_root, None
     else:
-        return ferst_root, second_root
+        return first_root, second_root
